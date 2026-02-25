8da33141292cb4bd6298080d737eb604861571ed
Kayshoo Nagar <ksunagar@gmail.com>
2026-02-25 18:38:18 +0530

    Program to calculate area of circle

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float radius;
+    cout << "Enter radius of the circle: ";
+    cin >> radius;
+    cout << "Areof the circle of radius: " << radius << " is:" << 3.14 * radius * radius;
+    return 0;
+}
--- practice_programs.cpp
+++ practice_programs.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 using namespace std;
 int main()
+// {
+//     cout << "hello" << "\n"
+//          << "Kayshoo";
+//     return 0;
+// }
+// {
+//     int x;
+//     int y;
+//     x = 5;
+//     y = 9;
+//     cout << x + y << endl
+//          << x * y;
+//     return 0;
+// }
+// {
+//     int x = 5;
+//     cout << x << endl;
+//     x = 7;
+//     cout << x << endl;
+//     x += 6;
+//     cout << x << endl;
+//     x -= 20;
+//     cout << x << endl;
+// }
+// {
+//     int x = 5, y = 2;
+//     cout << x + y << endl;
+//     cout << x - y << endl;
+//     cout << x * y << endl;
+//     cout << x / y << endl;
+// }
+// {
+//     float x = 3.14, y = 2.6;
+//     cout << x + y << endl;
+//     cout << x - y << endl;
+//     cout << x * y << endl;
+//     cout << x / y << endl;
 {
-    cout << "Hello World!";
-    return 0;
+    int x = 4;
+    cout << x << endl;
+    ++x;
+    cout << x << endl;
 }
